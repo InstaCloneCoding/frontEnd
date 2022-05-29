45e91ebe23486237640623534d0b361d14eaad6b
geol2 <big9401@gmail.com>
2022-05-29 19:00:04 +0900

    로그인 폼 중복 수정 적용

--- app/Views/main.php
+++ app/Views/main.php
@@ -35,23 +35,6 @@
                 <img src="/images/google-btn-img.png" alt="" style="width:135px;height:40px;">
             </div>
         </div>
-        <!-- // 로그인 로고 -->
-        <!-- ID / PW -->
-        <form method="post" id="form" name="form">
-            <label for="userId"><input type="text" id="userId" name="userId" placeholder="전화번호, 사용자 이름 또는 이메일" value=""/></label>
-            <label for="userPassword"><input type="password" id="userPassword" class="mt10" name="userPassword" placeholder="비밀번호" value="" /></label>
-        </form>
-        <!-- // ID / PW -->
-        <button onclick="fLogin();" class="login-btn btnM btnBlue w100 mt10">
-            <span id="loading">
-                <img class="spinner" src="/images/spinner_2.gif" style="width: 30px; height: 30px;">
-            </span>
-            로그인
-        </button>
-        <!-- 비밀번호를 잊으셨나요? -->
-        <div class="pw-find-box">
-            <a href="/accounts/password/reset"> 비밀번호를 잊으셨나요? </a>
-        </div>
     </div>
 </section>
 
