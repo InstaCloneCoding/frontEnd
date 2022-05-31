d02d25a4f4e11bbf37f10092766d7774a51bb618
Geol <big9401@gmail.com>
2022-05-31 16:57:22 +0900

    input 클릭 시, 줌 막기!

--- app/Views/header/header.php
+++ app/Views/header/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>Instagram</title>
     <meta name="description" content="The small framework with powerful features">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
     <link rel="stylesheet" href="/style/common.css">
     <link rel="stylesheet" href="/style/reset.css">
