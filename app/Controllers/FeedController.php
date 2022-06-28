d890c5875023eb3ea88fb7910fe089a7b6ee20bb
geol2 <big9401@gmail.com>
2022-06-29 00:28:40 +0900

    게시글 추가과정 추가

--- app/Controllers/FeedController.php
+++ app/Controllers/FeedController.php
@@ -5,19 +5,39 @@ namespace App\Controllers;
 use App\Libraries\curl\Curl;
 use App\Libraries\define\Define as Define;
 use App\Libraries\file\FileSave as FileSave;
+use CodeIgniter\API\ResponseTrait;
 
 class FeedController extends BaseController
 {
+    use ResponseTrait;
+
     public function content() {
         $post = $this->request->getPost();
         $files = $this->request->getFiles();
 
-        if( $files ) {
-            $result = FileSave::saveFiles($files);
+        if( !$files ) {
+            return $this->fail(["msg" => ""]);
         }
 
-        //$result = Curl::curlPost(Define::setAPIServer()."/content", $post);
-        $result = '';
+        $file = FileSave::saveFiles($files);
+
+        $sendFile = new \stdClass();
+        $sendFile->filePath = $file['fileInput']->getClientName();
+        $sendFile->fileSize = $files['fileInput']->getSize();
+        $sendFile->fileMimeType = $files['fileInput']->getMimeType();
+        $sendFile->fileName = $files['fileInput']->getName();
+
+        $sendData = new \stdClass();
+        $sendData->userId = $post['userId'];
+        $sendData->feedContent = $post['newPostData'];
+
+        $isSendData = Curl::curlPost(Define::setAPIServer()."/content/data", $sendData);
+
+
+        $isSendFile = Curl::curlPost(Define::setAPIServer()."/content/sendFile", $sendFile);
+
+
+
         return redirect()->to("/");
     }
 }
