d75b04fb89c2d518d3ea1eaa1218c9d3973abdf3
rhtjdrhkd123 <rhtjdrhkd123@naver.com>
2022-07-05 01:33:14 +0900

    마이페이지 디자인 작업

--- app/Views/userPage/userPage.php
+++ app/Views/userPage/userPage.php
@@ -31,11 +31,52 @@
             </div>
         </div>
     </div>
+    <!-- 탭 메뉴 -->
     <div class="tabMenu">
         <ul>
             <li class="active"><span class="material-symbols-outlined">grid_view</span>게시물</li>
             <li><span class="material-icons">bookmark_border</span> 저장됨</li>
         </ul>
     </div>
+    <!-- // 탭 메뉴 -->
+
+    <!-- 게시물 -->
+    <div class="tabContent content1">
+        <div class="userBoardWrap">
+            <div class="board">dddd</div>
+            <div class="board">dddd</div>
+            <div class="board">d</div>
+        </div>
+        <div class="userBoardWrap">
+            <div class="board">dddd</div>
+            <div class="board">dddd</div>
+            <div class="board">d</div>
+        </div>
+        <div class="userBoardWrap">
+            <div class="board">dddd</div>
+            <div class="board">dddd</div>
+            <div class="board">d</div>
+        </div>
+    </div>
+    <!-- // 게시물 -->
+
+    <!-- 저장됨 -->
+    <div class="tabContent content2">
+        <div class="contentTop col-between">
+            <div class="left">저장된 내용은 회원님만 볼 수 있습니다.</div>
+            <div class="right">+ 새 컬렉션</div>
+        </div>
+        <div class="noneContentWrap">
+            <div><img src="/images/collection-img.png" alt=""></div>
+            <div class="title">저장</div>
+            <div class="txt">
+                <p>다시 보고 싶은 사진과 동영상을 저장하세요. 콘텐츠를 </p>
+                <p>저장해도 다른 사람에게 알림이 전송되지 않으며, 저장된 </p>
+                <p>콘텐츠는 회원님만 볼 수 있습니다. </p>
+            </div>
+        </div>
+    </div>
+    <!-- // 저장됨 -->
+
 </section> 
 <script type="text/javascript" src="/script/feed.js"></script>
