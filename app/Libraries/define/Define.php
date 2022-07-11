6774e40825e25358f00f42a7ff83c2c945a916d7
백인걸 <big9401@gmail.com>
2022-07-11 23:54:28 +0900

    API 요청부 수정작성

--- app/Libraries/define/Define.php
+++ app/Libraries/define/Define.php
@@ -4,15 +4,19 @@ namespace App\Libraries\define;
 
 class Define
 {
-    public $API_SERVER;
+    private static string $API_SERVER = "http://52.79.54.87:8081";
+
+    private static string $LOCAL_SERVER = "http://localhost:8081";
 
     public function __construct() {
+        log_message("info", "API 요청 생성자");
     }
 
     public static function setAPIServer() {
-          // $API_SERVER = "http://localhost:8081";
-         $API_SERVER = "http://52.79.54.87:8081";
-
-        return $API_SERVER;
+        if(ENVIRONMENT == 'development') {
+            return self::$API_SERVER;
+        } else if(ENVIRONMENT == 'production') {
+            return self::$LOCAL_SERVER;
+        }
     }
 }
